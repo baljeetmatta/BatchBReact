346e5554d0f546d31d6e168b15903957c8221871
Baljeet Matta <baljeetmatta@Baljeets-MacBook-Air.local>
2025-02-04 11:15:19 +0530

    04022025

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+//import React from 'react'
+function app() {
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        Welcome to React
+      </div>
+      <p>Hello</p>
+    </>
+  )
+}
+function Test()
+{
+  return (
+    <>Test Component</>
+  )
+}
+
+export default app
+export {Test}
